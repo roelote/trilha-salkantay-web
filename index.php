854659689614ff54b1ab752292a47c3e25edef35
webmaster antisuyo <webmaster.antisuyo@gmail.com>
2019-02-08 10:26:15 -0500

    one commit

---
+++ index.php
@@ -0,0 +1,314 @@
+<?php 
+$final = $_SERVER["REQUEST_URI"];
+$url = 'https://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+
+$title_tour="";
+ ?>
+
+<!doctype html>
+<html lang="pt-BR">
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Trilha Salkantay | Trilha Inca Cuzco | Trilha Inca Machu Picchu</title>
+    <meta content="A Trilha Inca ou  Trilha Inca Cuzco é o trekking mais famoso da América do Sul e é classificado por muitos como uma das 5 melhores trilhas do mundo. Uma experiência única e quem o fez tem a certeza de que o esforço vale a pena! " name="description" />
+
+    <meta http-equiv="content-language" content="pt_BR" />
+    <meta name="content-language" content="pt-BR" />
+
+        <!-- open graph -->
+    <meta property="og:locale" content="pt_BR" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Trilha Salkantay | Trilha Inca Cuzco | Trilha Inca Machu Picchu" />
+    <meta property="og:description" content="A Trilha Inca ou  Trilha Inca Cuzco é o trekking mais famoso da América do Sul e é classificado por muitos como uma das 5 melhores trilhas do mundo. Uma experiência única e quem o fez tem a certeza de que o esforço vale a pena! " />
+    <meta property="og:url" content="https://www.trilhasalkantay.com/" />
+    <meta property="og:site_name" content="Trilha Inca Cuzco" />
+    <meta property="og:image" content="https://www.trilhasalkantay.com/img/trilhasalkantay.jpg" />
+
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    
+      <!-- favicon -->
+    <link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
+  </head>
+  <body>
+<!-- aside -->
+
+<div id="wrapper">
+  <section class="top-page">
+  <?php include('includes/header.php') ?>
+  </section>
+  
+  <div id="page-content-wrapper">
+
+      
+        <section class="tours-home container mt-1 mt-sm-3 bg-light">
+          <div class="row">
+            <div class="col-12 col-sm-6 col-md-3 p-1">
+               <a href="trilhaincaclassica.php">
+                  <div class="card mb-2 shadow-sm">
+                        <div class="card-header nc-color p-1">
+                           <h2>Trilha Inca Classica 4d / 3n</h2>
+                        </div>
+                        <div class="cardimg">
+                          <img class="card-img-top" src="img/box-home/trilhaincaclassica.jpg" alt="Card image cap">
+                        </div>
+                        <div class="card-body px-1 pb-0 pt-1">
+                          <p class="card-text mb-1 text-dark">
+                            5 dias 4 noites<br>
+                              A partir de <strong>USD 510 </strong>
+                          </p>
+                          <span class="badge bg-danger text-white float-right">Saiba Mais</span>
+                        </div>
+
+                </div>
+               </a>
+            </div>
+            <div class="col-12 col-sm-6 col-md-3 p-1">
+               <a href="trilhasalkantay.php">
+                  <div class="card mb-2 shadow-sm">
+                        <div class="card-header nc-color p-1">
+                           <h2>Trilha Salkantay 5d / 4n</h2>
+                        </div>
+                        <div class="cardimg">
+                          <img class="card-img-top" src="img/box-home/trilhasalkantay.jpg" alt="Card image cap">
+                        </div>
+                        <div class="card-body px-1 pb-0 pt-1">
+                          <p class="card-text mb-1 text-dark">
+                            5 dias 4 noites<br>
+                              A partir de <strong>USD 480 </strong>
+                          </p>
+                          <span class="badge bg-danger text-white float-right">Saiba Mais</span>
+                        </div>
+
+                </div>
+               </a>
+            </div>
+                  <div class="col-12 col-sm-6 col-md-3 p-1">
+               <a href="inca-jungle.php">
+                  <div class="card mb-2 shadow-sm">
+                        <div class="card-header nc-color p-1">
+                           <h2>Trilha Inca Jungle 4d / 3n</h2>
+                        </div>
+                        <div class="cardimg">
+                          <img class="card-img-top" src="img/box-home/trilhaincajungle.jpg" alt="Card image cap">
+                        </div>
+                        <div class="card-body px-1 pb-0 pt-1">
+                          <p class="card-text mb-1 text-dark">
+                            4 dias 3 noites<br>
+                              A partir de <strong>USD 380 </strong>
+                          </p>
+                          <span class="badge bg-danger text-white float-right">Saiba Mais</span>
+                        </div>
+
+                </div>
+               </a>
+            </div>
+            <div class="col-12 col-sm-6 col-md-3 p-1">
+               <a href="trilhaantisuyomachupicchu.php">
+                  <div class="card mb-2 shadow-sm">
+                        <div class="card-header nc-color p-1">
+                           <h2>Trilha Antisuyo 2d / 1n</h2>
+                        </div>
+                        <div class="cardimg">
+                          <img class="card-img-top" src="img/box-home/trilhaantisuyo.jpg" alt="Card image cap">
+                        </div>
+                        <div class="card-body px-1 pb-0 pt-1">
+                          <p class="card-text mb-1 text-dark">
+                            2 dias 1 noites<br>
+                              A partir de <strong>USD 335 </strong>
+                          </p>
+                          <span class="badge bg-danger text-white float-right">Saiba Mais</span>
+                        </div>
+
+                </div>
+               </a>
+            </div>
+            <div class="col-12 col-sm-6 col-md-3 p-1">
+               <a href="trilhaincacurta.php">
+                  <div class="card mb-2 shadow-sm">
+                        <div class="card-header nc-color p-1">
+                           <h2>Trilha Inca Curta 2d / 1n</h2>
+                        </div>
+                        <div class="cardimg">
+                          <img class="card-img-top" src="img/box-home/trilhaincacurta.jpg" alt="Card image cap">
+                        </div>
+                        <div class="card-body px-1 pb-0 pt-1">
+                          <p class="card-text mb-1 text-dark">
+                            2 dias 1 noites<br>
+                              A partir de <strong>USD 490 </strong>
+                          </p>
+                          <span class="badge bg-danger text-white float-right">Saiba Mais</span>
+                        </div>
+
+                </div>
+               </a>
+            </div>
+            <div class="col-12 col-sm-6 col-md-3 p-1">
+               <a href="cuscoclassicotradicional.php">
+                  <div class="card mb-2 shadow-sm">
+                        <div class="card-header nc-color p-1">
+                           <h2>Pacote Cusco  Tradicional</h2>
+                        </div>
+                        <div class="cardimg">
+                          <img class="card-img-top" src="img/box-home/cuscoclassicotradicional.jpg" alt="Card image cap">
+                        </div>
+                        <div class="card-body px-1 pb-0 pt-1">
+                          <p class="card-text mb-1 text-dark">
+                            4 dias 1 noites<br>
+                              A partir de <strong>USD 410 </strong>
+                          </p>
+                          <span class="badge bg-danger text-white float-right">Saiba Mais</span>
+                        </div>
+
+                </div>
+               </a>
+            </div>
+            <div class="col-12 col-sm-6 col-md-3 p-1">
+               <a href="cuscoclassicarqueologico.php">
+                  <div class="card mb-2 shadow-sm">
+                        <div class="card-header nc-color p-1">
+                           <h2>Pacote Cusco  Arqueológico</h2>
+                        </div>
+                        <div class="cardimg">
+                          <img class="card-img-top" src="img/box-home/cuscoclassicoarqueologico.jpg" alt="Card image cap">
+                        </div>
+                        <div class="card-body px-1 pb-0 pt-1">
+                          <p class="card-text mb-1 text-dark">
+                            5 dias 4 noites<br>
+                              A partir de <strong>USD 410 </strong>
+                          </p>
+                          <span class="badge bg-danger text-white float-right">Saiba Mais</span>
+                        </div>
+
+                </div>
+               </a>
+            </div>
+            <div class="col-12 col-sm-6 col-md-3 p-1">
+               <a href="cuscoclassicimperial.php">
+                  <div class="card mb-2 shadow-sm">
+                        <div class="card-header nc-color p-1">
+                           <h2>Pacote Cusco Imperial</h2>
+                        </div>
+                        <div class="cardimg">
+                          <img class="card-img-top" src="img/box-home/cuscoclassicoimperial.jpg" alt="Card image cap">
+                        </div>
+                        <div class="card-body px-1 pb-0 pt-1">
+                          <p class="card-text mb-1 text-dark">
+                            6 dias 5 noites<br>
+                              A partir de <strong>USD 450 </strong>
+                          </p>
+                          <span class="badge bg-danger text-white float-right">Saiba Mais</span>
+                        </div>
+
+                </div>
+               </a>
+            </div>
+            
+          </div>
+
+      </section>
+        <section class="container bg-light shadow mt-3">
+            <a href="https://www.trilhaincacuzco.com/trilhaincadisponibilidade.php"><img src="img/pacotesperu2.gif"  class="img-fluid d-block mx-auto" alt="logo"></a>
+            <hr> 
+            <div class="row">
+                <div class="col-12 col-sm-9">
+                  <h1>Trilha Salkanty Machu Picchu</h1>
+                
+                <div class="jumbotron p-3">
+                    <small>TRILHA INCA CUZCO é uma marca da NC TRAVEL CUSCO , agência de viagens e operador turístico autorizada a operar a rede da Trilha Inca Machu Picchu com código de autorização "CI1524" otorgado pelo Ministério da Cultura do Peru, veja nosso cadastro nesse link: <a href="http://www.machupicchu.gob.pe" target="_blank">www.machupicchu.gob.pe</a></small>
+                </div>
+
+                <h2>  Reserve o seu passeio para Trilha Inca Machu picchu</h2>
+
+                <img class="img-thumbnail float-right ml-2" src="img/trilha-inca-peru.jpg" alt="trilha inca" title="Trilha Inca Machu Picchu"><p>Veja a <a href="http://www.trilhaincacuzco.com/trilhaincadisponibilidade.php" title="disponibilidade das vagas a Trilha Inca Machu picchu">disponibilidade das vagas a Trilha Inca Machu picchu</a></p>
+
+                <p>A <strong title="Trilha Inca Machu Picchu">Trilha Inca Machu Picchu</strong> está considerada, como uma da <strong>Trilha Inca Machu Picchu</strong> mais famosa na América do Sul e o quarto no mundo. Para conhecer a <strong>Trilha Inca Machu Picchu</strong> é necessário fazer a reserva com bastante tempo de antecedência, a causa do limite das vagas a <strong>Trilha Inca</strong>.</p>
+
+                <p>Para fazer a <strong>Trilha Inca Machu Picchu</strong> temos duas opções como; <strong>Trilha Inca Machu Picchu Curta 2 Dias e 1 Noite</strong>, tambem a <strong>Trilha Inca Machu Picchu 4 Dias e 3 Noites</strong>.</p>
+                <img class="img-thumbnail float-left mr-2" src="img/tours-peru.jpg" alt="Trilha Inca Machu Picchu" title="Trilha Inca Machu Picchu">
+                <p>A <strong>Trilha Inca Machu Picchu</strong> fornece um roteiro detalhado e acessível para resolver suas dúvidas rapidamente e tambem para a <strong>Trilha Inca Machu Picchu</strong> é necessário que enviem seus dados corretos para realizar a reserva, porque não se pode fazer cambio de nomes e data de viagem a <strong>Trilha Inca Machu Picchu</strong>.</p>
+                <p>A única <strong>Trilha Inca Machu Picchu</strong> que chegam diretamente ao complexo arqueológico de <strong>Machu Picchu</strong> é <strong>Trilha Inca Machu Picchu 2 dias</strong> e <strong>Trilha Inca Machu Picchu 4 dias</strong>, alem disso existem milhares de quilômetros de &quot;<strong>TRILHA INCA</strong>&quot; em todo Cusco; no entanto, vamos nos concentrar no <strong>Trilha Inca Machu Picchu 4 dias</strong> que começa no km 82 da ferroviária de <strong>Cusco - Machu Picchu</strong> e a outra <strong>Trilha Inca Machu Picchu 2 dias</strong> que começa no quilômetro 104 da rota de <strong>Cusco - Machu Picchu</strong>.</p>
+
+                <p>A <strong>Trilha Inca Machu Picchu</strong> é uma rota em que se ligam muitos ecossistemas com uma sequência de monumentos da mais fina arquitetura na <strong>Trilha Inca</strong>, terminando com a clássica vista de postal de <strong>Machu Picchu</strong> visto da cima da montanha de <strong>Machu Picchu</strong>. A <strong>Trilha Inca Machu Picchu</strong> é uma rota de peregrinação e de purificação para entrar no <strong>Sagrada povo de Pachacutec</strong> (Llacta de Pachacutec); são 44 quilômetros da <strong>Trilha Inca Machu Picchu de 4 dias</strong> e 18 quilometros da <strong>Trilha Inca Machu Picchu 2 dias</strong>, na <strong>Trilha Inca Machu Picchu</strong> pode ser visto túneis escavados e muitas escadas pela cima das montanhas de <strong>Machu Picchu</strong>.</p>
+                <p>Para a <strong>Trilha Inca Machu Picchu</strong> é necessário ter um bom preparo físico, a causa da altitude e as subidas ate ponto mais alto da <strong>Trilha Inca Machu Picchu</strong>.</p>
+                
+                <div class="text-center">
+                   <small>               
+                      <ul class="list-inline">
+                        <li class="list-inline-item"> <a href="trilhainca.php" title="Trilha Inca">Trilha Inca</a></li>
+                        <li class="list-inline-item"><a href="trilhaincacurta.php" title="Trilha Inca Cusco">Trilha Inca Cusco</a></li>
+                        <li class="list-inline-item"><a href="salkantay.php" title="Trilha Salkantay">Trilha Salkantay</a></li>
+                        <li class="list-inline-item"><a href="machupicchu.php" title="Machu Picchu">Machu Picchu</a></li>
+                        <li class="list-inline-item"><a href="trilhaincaclassica.php" title="Trilha Inca Machu Picchu">Trilha Inca e Machu Picchu</a></li>
+                    </ul>
+                  </small>
+                </div>
+
+                <hr>
+
+                <div class="pacotes-home">
+                        <a href="cuscopunoalternativo.php">
+                            <img class="img-thumbnail w-100" src="img/pacotes_peru_1.jpg" alt="pacotes peru">
+                             <div class="text-center nc-color text-light"><span>MAIS INFORMAÇÕES ...</span></div>
+                        </a>
+                </div>
+                <div class="pacotes-home">
+                        <a href="lima-cusco-puno.php">
+                            <img class="img-thumbnail w-100" src="img/pacotes_peru_2.jpg" alt="pacotes peru">
+                             <div class="text-center nc-color text-light"><span>MAIS INFORMAÇÕES ...</span></div>
+                        </a>
+                </div>
+                <div class="pacotes-home">
+                        <a href="pacotetudoperu.php">
+                            <img class="img-thumbnail w-100" src="img/pacotes_peru_3.jpg" alt="pacotes peru">
+                             <div class="text-center nc-color text-light"><span>MAIS INFORMAÇÕES ...</span></div>
+                        </a>
+                </div>
+
+
+                </div>
+                <div class="col-12 col-sm-3">
+                  <?php include('includes/aside.php') ?>
+                </div>
+            </div>
+        </section>
+
+
+        <section class="mt-2 bot-page">
+              <?php include('includes/footer.php') ?>
+        </section>
+  </div>
+  
+</div>
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" ></script>
+    <script src="js/script.js"></script>
+      
+
+  </body>
+</html>
